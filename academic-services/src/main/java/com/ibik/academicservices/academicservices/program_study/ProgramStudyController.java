03af8e7eeef86359240a03477fa35ab974799ec1
YudhistiraKusuma202310067 <202310067@student.ibik.ac.id>
2022-10-19 22:33:17 +0700

    Validation and Handler

---
+++ academic-services/src/main/java/com/ibik/academicservices/academicservices/program_study/ProgramStudyController.java
@@ -0,0 +1,133 @@
+package com.ibik.academicservices.academicservices.program_study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibik.academicservices.academicservices.dto.ResponseData;
+
+@RestController
+@RequestMapping("/api/program_study")
+public class ProgramStudyController {
+
+    @Autowired
+    private ProgramStudyServices programStudyServices;
+
+    @PostMapping
+    public ResponseEntity<ResponseData<ProgramStudy>> posProgramStudy(@Valid @RequestBody ProgramStudy programstudy,
+            Errors errors) {
+        ResponseData<ProgramStudy> responseData = new ResponseData<>();
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+
+            responseData.setResult(false);
+            responseData.setData(null);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+
+        responseData.setResult(true);
+        List<ProgramStudy> value = new ArrayList<>();
+        value.add(programStudyServices.save(programstudy));
+        responseData.setData(value);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseData<ProgramStudy>> fetchProgramStudy() {
+        ResponseData<ProgramStudy> responseData = new ResponseData<>();
+        try {
+            responseData.setResult(true);
+            List<ProgramStudy> value = (List<ProgramStudy>) programStudyServices.findAll();
+            responseData.setData(value);
+
+            return ResponseEntity.ok(responseData);
+        } catch (Exception ex) {
+            responseData.setResult(false);
+            responseData.getMessage().add(ex.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseData<ProgramStudy>> fetchProgramStudyById(@PathVariable("id") int id) {
+        ResponseData<ProgramStudy> responseData = new ResponseData<>();
+        try {
+            responseData.setResult(true);
+            List<ProgramStudy> value = new ArrayList<>();
+            value.add(programStudyServices.findOne(id));
+            responseData.setData(value);
+
+            return ResponseEntity.ok(responseData);
+        } catch (Exception ex) {
+            responseData.setResult(false);
+            responseData.getMessage().add(ex.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseData<ProgramStudy>> updateProgramStudy(@Valid @RequestBody ProgramStudy programstudy,
+            Errors errors) {
+        ResponseData<ProgramStudy> responseData = new ResponseData<>();
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+
+            responseData.setResult(false);
+            responseData.setData(null);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        try {
+            responseData.setResult(true);
+            List<ProgramStudy> value = new ArrayList<>();
+            value.add(programStudyServices.update(programstudy));
+            responseData.setData(value);
+
+            return ResponseEntity.ok(responseData);
+        } catch (Exception ex) {
+            responseData.getMessage().add("ID is Required");
+            responseData.setResult(false);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseData<Void>> deleteProgramStudyById(@PathVariable("id") int id) {
+        ResponseData<Void> responseData = new ResponseData<>();
+        try {
+            programStudyServices.removeOne(id);
+            responseData.setResult(true);
+            responseData.getMessage().add("Successfully Remove");
+
+            return ResponseEntity.ok(responseData);
+        } catch (Exception ex) {
+            responseData.setResult(false);
+            responseData.getMessage().add(ex.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+    }
+}
---
+++ academic-services/src/main/java/com/ibik/academicservices/academicservices/programs/ProgramsController.java
@@ -0,0 +1,131 @@
+package com.ibik.academicservices.academicservices.programs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibik.academicservices.academicservices.dto.ResponseData;
+
+@RestController
+@RequestMapping("/api/programs")
+public class ProgramsController {
+    
+    @Autowired
+    private ProgramsServices programsServices;
+
+    @PostMapping
+    public ResponseEntity<ResponseData<Programs>> postProgram(@Valid @RequestBody Programs programs, Errors errors) {
+        ResponseData<Programs> responseData = new ResponseData<>();
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+
+            responseData.setResult(false);
+            responseData.setData(null);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+
+        responseData.setResult(true);
+        List<Programs> value = new ArrayList<>();
+        value.add(programsServices.save(programs));
+        responseData.setData(value);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseData<Programs>> fetchPrograms() {
+        ResponseData<Programs> responseData = new ResponseData<>();
+        try {
+            responseData.setResult(true);
+            List<Programs> value = (List<Programs>) programsServices.findAll();
+            responseData.setData(value);
+
+            return ResponseEntity.ok(responseData);
+        } catch (Exception ex) {
+            responseData.setResult(false);
+            responseData.getMessage().add(ex.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseData<Programs>> fetchProgramsById(@PathVariable("id") int id) {
+        ResponseData<Programs> responseData = new ResponseData<>();
+        try {
+            responseData.setResult(true);
+            List<Programs> value = new ArrayList<>();
+            value.add(programsServices.findOne(id));
+            responseData.setData(value);
+
+            return ResponseEntity.ok(responseData);
+        } catch (Exception ex) {
+            responseData.setResult(false);
+            responseData.getMessage().add(ex.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseData<Programs>> updateProgram(@Valid @RequestBody Programs programs, Errors errors) {
+        ResponseData<Programs> responseData = new ResponseData<>();
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+
+            responseData.setResult(false);
+            responseData.setData(null);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        try {
+            responseData.setResult(true);
+            List<Programs> value = new ArrayList<>();
+            value.add(programsServices.update(programs));
+            responseData.setData(value);
+
+            return ResponseEntity.ok(responseData);
+        } catch (Exception ex) {
+            responseData.getMessage().add("ID is Required");
+            responseData.setResult(false);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseData<Void>> deleteProgramsById(@PathVariable("id") int id) {
+        ResponseData<Void> responseData = new ResponseData<>();
+        try {
+            programsServices.removeOne(id);
+            responseData.setResult(true);
+            responseData.getMessage().add("Successfully Remove");
+
+            return ResponseEntity.ok(responseData);
+        } catch (Exception ex) {
+            responseData.setResult(false);
+            responseData.getMessage().add(ex.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+    }
+}
